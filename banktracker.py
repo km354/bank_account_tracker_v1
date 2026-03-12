870ad84daeff8fdece867f0c8a63d5b4c1867170
Kaden Mayberry <kdmayberry01@gmail.com>
2026-03-11 20:58:03 -0500

    first commit

---
+++ banktracker.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import csv
+
+class BankTracker:
+    def __init(self):
+        self.trackmoney = []
+
+    def withdraw_money:
+        pass
+
+    def deposit_money:
+        pass
