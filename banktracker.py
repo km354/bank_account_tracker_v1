c611b2f56c6740d49a156fd8040eacbb83c3050d
Kaden Mayberry <kdmayberry01@gmail.com>
2026-03-12 20:02:54 -0500

    fixed errors with printing when user selects deposit or withdraw money

--- banktracker.py
+++ banktracker.py
@@ -12,31 +12,38 @@ class BankTracker:
             total += money
         return total
 
-    def withdraw_money(self, amount):
+    def withdraw_money(self):
         amount = input("Enter Amount: ")
-        if amount.isdigit():
-            self.trackmoney.remove(amount)
-            self.history.append(f' -{amount}')
-            print(f"Your withdraw of ${amount} was successful")
-        else:
-            print("Error. Make sure there is only numbers")
+        try:
+            amount = float(amount)
+            if amount > self.bank_balance():
+                print("Error. Insufficient funds.")
+            else:
+                self.trackmoney.remove(amount)
+                self.history.append(f' -{amount}')
+                print(f"Your withdraw of ${amount} was successful")
+        except ValueError:
+            print("Error. Please enter a valid number.")
 
-    def deposit_money(self, amount):
+    def deposit_money(self):
         amount = input("Enter amount: ")
-        if amount.isdigit():
+        try:
+            amount = float(amount)
             self.trackmoney.append(amount)
             self.history.append(f'+ {amount}')
             print(f"Your deposit of ${amount} was successful")
             print(f"Your current balance is ${self.bank_balance()}")
-        else:
-            print("Error. Make sure to only have numbers")
-            return
-
+            if amount <= 0:
+                print("Error. You must deposit more than $0")
+                return
+        except ValueError:
+            ("Error. Please try again. Numbers only")
+            
     def view_transaction_history(self):
         print(self.history)
         
     def view_bank_balance(self):
-        print(self.trackmoney)
+        print(f"Your bank balance is ${self.bank_balance()}")
 
 def main():
     bank = BankTracker()
@@ -50,11 +57,11 @@ def main():
 
         choice = input("Enter Choice:")
         if choice == "1" or choice.lower() == "deposit money":
-            bank.deposit_money(None)
+            bank.deposit_money()
         elif choice == "2" or choice.lower() == "withdraw money":
-            bank.withdraw_money(None)
+            bank.withdraw_money()
         elif choice == "3" or choice.lower() == "view account balance":
-            bank.bank_balance()
+            bank.view_bank_balance()
         elif choice == "4" or choice.lower() == "view transaction history":
             bank.view_transaction_history()
         elif choice == "5" or choice.lower() == "leave the bank":
